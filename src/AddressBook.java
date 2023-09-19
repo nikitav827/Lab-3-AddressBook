c38606abb48b72b52cfe63980e473c46902583d3
nikita <nikitasaravijay@cmail.carleton.ca>
2023-09-19 19:18:36 -0400

    Added and removed buddy from buddyInfoList.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,6 +17,12 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+
+        BuddyInfo buddy = new BuddyInfo("Tom");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+
     }
 
 
